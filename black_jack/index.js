2cb2159e573572804150991db66918c3d517fb93
jcovington16 <covingtonjoshua7@gmail.com>
2021-07-04 14:01:40 -0600

    created an object that has the user's money bag. want to use this object to simulate a user spending money in order to play.

--- black_jack/index.js
+++ black_jack/index.js
@@ -10,6 +10,14 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+let player = {
+    name : "User",
+    chips : 145
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
 function getRandomCard() {
     let randomCard = Math.floor( Math.random() * 13) + 1
 
