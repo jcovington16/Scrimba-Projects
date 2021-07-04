281ed51de14d8493e40c262d829df9e86bd2c54d
jcovington16 <covingtonjoshua7@gmail.com>
2021-07-03 22:50:52 -0600

    linking my styling, adding more detail to my README file and now working on the generating new card function.

--- black_jack/README.md
+++ black_jack/README.md
@@ -1,5 +1,7 @@
 ## Black Jack APP
 
-- This is a black jack app using vanilla JavaScript and some basic styling. There's some DOM manipulation and simple math to make this game work.  
+- This is a black jack app using vanilla JavaScript and some basic styling. There's some DOM manipulation and simple math to make this game work.
+
+- I am getting a lot of practice with making my functions logical and flow naturally. I'm also trying to write as much clean code as possible and make it easy to read. 
 
 - Thanks to Scrimba and their JS Cohort I am able to practice my current skills and improve on my JS development. 
--- black_jack/index.js
+++ black_jack/index.js
@@ -2,27 +2,61 @@ let cards = []
 
 let lives = 3
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
-let sum
+let sum = 0
 
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
 function getRandomCard() {
-    return Math.floor( Math.random() * 13) + 1
+    let randomCard = Math.floor( Math.random() * 13) + 1
+
+    if (randomCard > 10) {
+        return 10
+    } else if (randomCard === 1) {
+        return 11
+    } else {
+        return randomCard
+    }
 }
 
 function startGame() {
+    isAlive = true
+
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+
+    cards = [firstCard, secondCard]
+
+    sum = firstCard + secondCard
+
     renderGame()
 }
 
 function renderGame() {
     cardsEl.textContent = "Cards: "
+
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
 
 function newCard() {
     let card = getRandomCard()
+
+    sum += card
 
 }
